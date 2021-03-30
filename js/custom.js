cfdf61f4696d9c5aa8a0aa80abefc6d2a27b1541
codze-fullstack <ahmed.codze@gmail.com>
2021-03-30 18:08:24 +0200

    'colors'

--- js/custom.js
+++ js/custom.js
@@ -1,84 +1,13 @@
-/*global $, alert, console*/
-
-$(function () {
-   
-    'use strict';
-    
-    
-    // Links Add Active Class
-    
-    $('.links li a').click(function () {
-       
-        $(this).parent().addClass('active').siblings().removeClass('active');
-        
+$(document).ready(function() {
+    $(".loading").slideUp('slow',function () {
+        $(".loading").remove()
     });
-    
-    // Adjust Bxslider List Item Center
-    
-    mySlider.each(function () {
-       
-        $(this).css('paddingTop', ($(window).height() - $('.bxslider li').height()) / 2);
-        
-    });
-    
-    // Our Auto Slider Code
-    
-    (function autoSlider() {
-        
-        $('.slider .active').each(function () {
-           
-            if (!$(this).is(':last-child')) {
-                
-                $(this).delay(3000).fadeOut(1000, function () {
-                   
-                    $(this).removeClass('active').next().addClass('active').fadeIn();
-                    
-                    autoSlider();
-                    
-                });
-                
-            } else {
-                
-                $(this).delay(3000).fadeOut(1000, function () {
-                    
-                    $(this).removeClass('active');
-                    
-                    $('.slider div').eq(0).addClass('active').fadeIn();
-                    
-                    autoSlider();
-                    
-                });
-                
-            }
-            
-        });
-        
-    }());
-    
-    // Trigger MixitUp
-    
-    $('#Container').mixItUp();
-    
-    // Adjust Shuffle Links
-    
-    $('.shuffle li').click(function () {
-       
-        $(this).addClass('selected').siblings().removeClass('selected');
-        
-    });
-    // hide and show logo 
-    $('.navbar-toggler').click(function () {
-        $(this).toggleClass('test');
-        if ($(this).hasClass('test')) {
-            $('.navbar-brand').addClass('hidden');
-        } else {
-        setTimeout(
-            function() 
-            {
-                $('.navbar-brand').removeClass('hidden');
-            },350);
-        }
-    });
+    $(".project").mouseenter(function () {
+        $(this).children(".overlay").show('slow')
+    })
+    $(".project").mouseleave(function () {
+        $(this).children(".overlay").hide('slow')
+    })
 
 
 });
